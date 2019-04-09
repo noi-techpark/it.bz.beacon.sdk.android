b9b75a3fc4cad96d444e19a8e20e97c8d11c7fcb
Johannes Andrich <johannes.andrich@raiffeisen.net>
2019-04-09 16:38:42 +0200

    Add config for pom file creation

--- beaconsuedtirolsdk/src/main/java/it/bz/beacon/beaconsuedtirolsdk/swagger/client/ApiClient.java
+++ beaconsuedtirolsdk/src/main/java/it/bz/beacon/beaconsuedtirolsdk/swagger/client/ApiClient.java
@@ -52,7 +52,7 @@ import it.bz.beacon.beaconsuedtirolsdk.swagger.client.auth.OAuth;
 
 public class ApiClient {
 
-    private String basePath = "https://api.beacon-dev.it";
+    private String basePath = "https://api.beacon.bz.it";
     private boolean debugging = false;
     private Map<String, String> defaultHeaderMap = new HashMap<String, String>();
     private String tempFolderPath = null;
@@ -107,7 +107,7 @@ public class ApiClient {
     /**
      * Set base path
      *
-     * @param basePath Base path of the URL (e.g https://api.beacon-dev.it
+     * @param basePath Base path of the URL (e.g https://api.beacon.bz.it
      * @return An instance of OkHttpClient
      */
     public ApiClient setBasePath(String basePath) {
